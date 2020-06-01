19af81c8e9e88d252351ae73c6b1d6c945f58952
antonio martinez baez <tono.mtzb@gmail.com>
2020-06-01 13:51:06 -0500

    updated nav

--- js/index.js
+++ js/index.js
@@ -39,4 +39,11 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navLinks = document.querySelectorAll('nav a');
+  navLinks.forEach((link, index) =>{
+    link.textContent = siteContent['nav'][`nav-item-${index+1}`];
+  });
+
+
